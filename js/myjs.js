b07f842af3859a86cac7a92fbcde0fa77afc77a2
warutlee <tomcheer2@hotmail.com>
2013-04-24 16:02:56 +1000

    qr
    qr

--- js/myjs.js
+++ js/myjs.js
@@ -8,14 +8,7 @@
 
 
 
-/*Example of a single page*/
-	/*$("#puttytemplate-home").bind("pageinit",function(event){
-		
-		$("#" + event.target.id).find("[data-role=footer][footer-nav=enabled]").load("footer.html", function(){
-                $("#" + event.target.id).find("[data-role=navbar]").navbar();
-            });
-			
-	});*/
+
 
 
 /*INIT FOOTER*/
@@ -31,74 +24,91 @@ $(document).bind("pageshow",function(event){
 });
 /*END INIT FOOTER*/
 
-/*Log in*/
-$("#login").bind("pageinit",function(){
-	
-	$("#login-submit").bind("click",function(){
-		var username = $("#login").find("#login-username").val();
-		var password = $("#login").find("#login-password").val();
-		
-		
-		$.cookie('username', username);
-		$.cookie('password', password);
-		
-		
-		
-		alert("submit ! \n"+$.cookie('username')+" \n"+$.cookie('password'));
-		if(username=="leew" && password == "leew"){
-			
-			$.mobile.changePage($("#test"));
-		}else{
-			alert("username and pass is wrong !!");
-			}
-		
-		
+$(document).bind('pageinit',function(){
+	$(".swipable").on("swiperight",function(){
+		alert("swipe left");
+		$("#left-panel").panel( "open");
 	});
-	
+	$(".swipable").on("swipeleft",function(){
+		alert("swipe right");
+		$("#right-panel").panel( "open");
+	});
 });
 
-
-/*QR SCANNER*/
-$("#puttytemplate-qr").bind("pageinit",function(){
-	
-	$("#qr-result").html("");
-	
-	/*INVOKER for the lastest version of phonegap*/
-	/*$("#puttytemplate-scanqr").bind('click',function(e){
+$(document).ready(function(e) {
+/*Log in*/
+	$("#login").bind("pageinit",function(){
 		
-		window.plugins.barcodeScanner.scan(
+		$("#login-submit").bind("click",function(){
+			var username = $("#login").find("#login-username").val();
+			var password = $("#login").find("#login-password").val();
 			
-			function(result) { 
-					alert("We got a barcode\n"+"Result: " + result.text + "\n" + "Format: " + result.format); 
-				}, 
-			function(error) { 
-				alert("Scanning failed: " + error); 
-			});
+			
+			$.cookie('username', username);
+			$.cookie('password', password);
+			
+			
+			
+			alert("submit ! \n"+$.cookie('username')+" \n"+$.cookie('password'));
+			if(username=="leew" && password == "leew"){
+				
+				$.mobile.changePage($("#test"));
+			}else{
+				alert("username and pass is wrong !!");
+				}
+			
+			
+		});
 		
-	});*/
-	
-	
-	
-	$("#puttytemplate-scanqr").bind('click',function(e){
-		window.plugins.barcodeScanner.scan(
-    		function(result) {
-       				if (result.cancelled){
-            			
-						$("#qr-result").text("You just cancelled it :'(");
-					}
-					
-        			else{
-            			
-						$("#qr-result").html("<h1><a href='"+result.text+"' data-rel='external' class='qr-result-link' >"+result.text+"</a>");
-					}
-    		},
-    		function(error) {
-        		alert("scanning failed: " + error);
-    		}
-		)
 	});
+
+
+	/*QR SCANNER*/
+	$("#puttytemplate-qr").bind("pageinit",function(){
+		
+		$("#qr-result").html("");
+		
+		/*INVOKER for the lastest version of phonegap*/
+		/*$("#puttytemplate-scanqr").bind('click',function(e){
+			
+			window.plugins.barcodeScanner.scan(
+				
+				function(result) { 
+						alert("We got a barcode\n"+"Result: " + result.text + "\n" + "Format: " + result.format); 
+					}, 
+				function(error) { 
+					alert("Scanning failed: " + error); 
+				});
+			
+		});*/
+		
+		
+		
+		$("#puttytemplate-scanqr").bind('click',function(e){
+			window.plugins.barcodeScanner.scan(
+				function(result) {
+						if (result.cancelled){
+							
+							$("#qr-result").text("You just cancelled it :'(");
+						}
+						
+						else{
+							
+							$("#qr-result").html("<h1><a href='"+result.text+"' data-rel='external' class='qr-result-link' >"+result.text+"</a>");
+						}
+				},
+				function(error) {
+					alert("scanning failed: " + error);
+				}
+			);
+			
+		
+		
+		
+		
+		});
 	
-	
-	
-	
+		
+	});
+
 });
